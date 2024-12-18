1bde90a1fa8366363e0c28b9eb861397ec81d962
abdelrahman <abdomohamed11417@gmail.com>
2024-12-18 20:38:42 +0200

    tunning designs

--- notch_filter_v3_prj/project.ads
+++ notch_filter_v3_prj/project.ads
@@ -1,4 +1,4 @@
-<ADSProject Revision="7" Version="100">
+<ADSProject Revision="14" Version="100">
     <Project Name="">
         <Design Name="CL_MS.dsn" />
         <Design Name="CL_TL.dsn" />
@@ -7,7 +7,6 @@
         <Data_Display Name="CL_MS.dds" />
         <Data_Display Name="CL_TL.dds" />
         <Data_Display Name="QR_TL.dds" />
-        <Dataset Name="CL_MS.ds" />
         <Dataset Name="CL_TL.ds" />
         <Dataset Name="QR_TL.ds" />
         <Log Name="momServer.log" />
@@ -18,14 +17,15 @@
         <Preferences Name="schematic.prf" />
         <Design Name="CL_MS_Tuned.dsn" />
         <Data_Display Name="CL_MS_Tuned.dds" />
-        <Dataset Name="CL_MS_Tuned.ds" />
         <Dataset Name="LR_MS.ds" />
-        <Design Name="LR_MS_v2.dsn" />
         <Dataset Name="LR_MS_v2.ds" />
-        <Data_Display Name="LR_MS_v2.dds" />
-        <Design Name="LR_MS_v3.dsn" />
-        <Data_Display Name="LR_MS_v3.dds" />
         <Dataset Name="LR_MS_v3.ds" />
+        <Dataset Name="CL_MS_Tuned.ds" />
+        <Dataset Name="CL_MS.ds" />
+        <Data_Display Name="LR_MS_v1.dds" />
         <Design Name="LR_MS_v1.dsn" />
+        <Dataset Name="LR_MS_v1.ds" />
+        <Data_Display Name="LR_MS_v2.dds" />
+        <Design Name="LR_MS_v2.dsn" />
     </Project>
 </ADSProject>
